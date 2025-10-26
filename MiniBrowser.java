da03cb290e9e37eccb24c6f076785a905ae71348
almiraira <almira.hardianti_ti25@nusaputra.ac.id>
2025-10-26 08:45:39 +0700

    Add files via upload

---
+++ MiniBrowser.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class MiniBrowser {
+    public static void main(String[] args) throws Exception {
+        Scanner inputx = new Scanner(System.in);
+        String answer = "1";
+        Stack<String>urls = new Stack<>();
+
+        System.out.println("\n1. Enter the url \n2. Back to previus url\n3. Show the current url \n4. Show the history of the urls visited \n5. Exit the program ");
+    
+        do {
+            System.out.print("\nEnter your choic (1-5) : ");
+            answer = inputx.nextLine();
+            if (answer.equals("1")) {
+                System.out.print("Enter URL : ");
+                String url = inputx.nextLine();
+                urls.push(url);
+            }else if (answer.equals("2")) {
+                if (urls.size() > 1) {
+                    urls.pop();
+                    System.out.println("current url :"+urls.peek());
+                }else{
+                    System.out.print("No way back to previous URL \n ");
+                }
+            }else if (answer.equals("3")) {
+                System.out.println("curret url :"+urls.peek());
+            }else if (answer.equals("4")) {
+                System.out.print(urls);
+            }else if (answer.equals("5")) {
+                System.out.print("exiting the program. goodbye! ");   
+            }
+        }while(!answer.equals("5"));
+        inputx.close();
+    }
+}
